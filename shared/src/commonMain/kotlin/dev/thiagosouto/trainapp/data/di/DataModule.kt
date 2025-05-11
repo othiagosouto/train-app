6599666b5d3334fc499bb8a5c091e02f5a82c7ee
Thiago Souto <soutosss@gmail.com>
2025-05-11 18:02:10 +0200

    Refactor modules

--- shared/src/commonMain/kotlin/dev/thiagosouto/trainapp/data/di/DataModule.kt
+++ shared/src/commonMain/kotlin/dev/thiagosouto/trainapp/data/di/DataModule.kt
@@ -1,14 +1,27 @@
 package dev.thiagosouto.trainapp.data.di
 
+import dev.thiagosouto.trainapp.data.DefaultTasksRepository
 import dev.thiagosouto.trainapp.data.TaskService
+import dev.thiagosouto.trainapp.data.TasksRepository
+import dev.thiagosouto.trainapp.data.local.TasksCache
+import dev.thiagosouto.trainapp.data.model.BaseUrl
 import dev.thiagosouto.trainapp.data.model.TasksBaseUrl
 import dev.thiagosouto.trainapp.data.remote.DefaultTaskService
 import dev.thiagosouto.trainapp.data.remote.serviceResponseValidator
+import dev.thiagosouto.trainapp.utils.AppDispatchers
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
 import org.koin.dsl.module
+
+val urlModule = {
+    module {
+        factory<BaseUrl> {
+            TasksBaseUrl
+        }
+    }
+}
 
 val dataModule = module {
     single {
@@ -25,11 +38,26 @@ val dataModule = module {
             serviceResponseValidator()
         }
     }
-    single<TaskService> {
+    factory<TaskService> {
         DefaultTaskService(
             json = get(),
             httpClient = get(),
-            baseUrl = TasksBaseUrl
+            baseUrl = get<BaseUrl>()
+        )
+    }
+    single<TasksCache> {
+        TasksCache()
+    }
+
+    factory<AppDispatchers> {
+        AppDispatchers()
+    }
+
+    factory<TasksRepository> {
+        DefaultTasksRepository(
+            taskService = get(),
+            tasksCache = get(),
+            dispatchers = get<AppDispatchers>()
         )
     }
 }
