25ea28e02058db68a3e8a6fe2d883476b7b551b9
Thiago Souto <soutosss@gmail.com>
2025-05-11 15:33:15 +0200

    Delete unnucessary class

--- shared/src/commonMain/kotlin/dev/thiagosouto/trainapp/domain/InvalidPriorityLevelException.kt
+++
@@ -1,8 +0,0 @@
-package dev.thiagosouto.trainapp.domain
-
-/**
- * Exception thrown when an invalid priority level is encountered.
- *
- * @param message The detail message for the exception.
- */
-class InvalidPriorityLevelException(message: String) : Exception(message)
